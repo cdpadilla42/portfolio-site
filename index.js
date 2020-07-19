9ec6d65c39da973108704c09f2aae3f759b3b72a
Chris Padilla <cdpadilla42@gmail.com>
2020-07-19 10:11:47 -0500

    Add animation to skills and tools with intersection observer

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const usesIcons = document.querySelector('.uses__icons');
 function animateOnScroll(element) {
   function toggleAnimation(payload) {
     console.log(payload);
-    if (payload[0].isIntersecting) console.log('We are on!');
-    if (!payload[0].isIntersecting) console.log('We are off!');
+    if (payload[0].isIntersecting) {
+      element.classList.remove('pre-animation');
+    }
+    if (!payload[0].isIntersecting) {
+      element.classList.add('pre-animation');
+    }
   }
   const ob = new IntersectionObserver(toggleAnimation);
   ob.observe(element);
