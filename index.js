eb3315944ad16c645da1406dda64226efa88c284
Chris Padilla <cdpadilla42@gmail.com>
2020-07-19 13:13:20 -0500

    Add intersection observing animation to hero text

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const usesIcons = document.querySelector('.uses__icons');
+const heroTextEl = document.querySelector('.hero__text_greeting');
+const heroTextTrigger = document.querySelector('.animation_prompt');
 
 function animateOnScroll(element) {
+  const ob = new IntersectionObserver(toggleAnimation);
+
   function toggleAnimation(payload) {
-    console.log(payload);
     if (payload[0].isIntersecting) {
       element.classList.remove('pre-animation');
-    }
-    if (!payload[0].isIntersecting) {
+    } else {
       element.classList.add('pre-animation');
     }
   }
-  const ob = new IntersectionObserver(toggleAnimation);
+
   ob.observe(element);
 }
 
+function animateOnLeaving(element, triggerEl) {
+  const ob = new IntersectionObserver(toggleAnimation);
+
+  function toggleAnimation(payload) {
+    console.log(payload[0].intersectionRatio);
+    if (payload[0].isIntersecting) {
+      element.classList.remove('pre-animation');
+    } else {
+      element.classList.add('pre-animation');
+    }
+  }
+
+  ob.observe(triggerEl, {
+    root: null,
+    threshold: 0,
+  });
+}
+
 animateOnScroll(usesIcons);
+animateOnLeaving(heroTextEl, heroTextTrigger);
